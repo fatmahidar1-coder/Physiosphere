40d5fea79c6a5b514aa505d711835034a5b86527
fatmahidar1-coder <fatmahidar1@gmail.com>
2026-07-25 01:26:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function openCourses() {
-    window.location.href = "courses.html";
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+45,
+1,
+0.1,
+1000
+);
+
+camera.position.z = 3;
+
+const renderer = new THREE.WebGLRenderer({
+alpha:true,
+antialias:true
+});
+
+renderer.setSize(320,320);
+
+document.getElementById("globe").appendChild(renderer.domElement);
 }
