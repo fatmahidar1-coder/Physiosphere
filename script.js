481dabdf177469e73bb2da45d8ce19b35d289718
fatmahidar1-coder <fatmahidar1@gmail.com>
2026-07-25 01:27:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ renderer.setSize(320,320);
 
 document.getElementById("globe").appendChild(renderer.domElement);
 }
+const geometry = new THREE.SphereGeometry(1,64,64);
+
+const texture = new THREE.TextureLoader().load("globe.png");
+
+const material = new THREE.MeshStandardMaterial({
+map:texture
+});
+
+const globe = new THREE.Mesh(
+geometry,
+material
+);
+
+scene.add(globe);
